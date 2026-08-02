94839feda9e93391c8966cb73f3fd1e3ce2e84f6
stojce <stojce@me.com>
2026-08-02 10:54:33 +0200

    fix: build

--- tests/release-please-config.sh
+++ tests/release-please-config.sh
@@ -5,9 +5,9 @@ config="release-please-config.json"
 manifest=".release-please-manifest.json"
 
 [[ "$(jq -r '.packages | keys | join(" ")' "$config")" == "." ]]
-[[ "$(jq -r '.packages["."].["release-type"]' "$config")" == "rust" ]]
+[[ "$(jq -r '.packages["."].["release-type"]' "$config")" == "go" ]]
 [[ "$(jq -r '.packages["."].["changelog-path"]' "$config")" == "backend/CHANGELOG.md" ]]
-[[ "$(jq -r '.packages["."].["extra-files"][]' "$config")" == "frontend/package.json" ]]
+[[ "$(jq -r '.packages["."].["extra-files"][] | [.type, .path, (.jsonpath // "")] | @tsv' "$config")" == $'toml\tbackend/Cargo.toml\t$.package.version\ntoml\tbackend/Cargo.lock\t$.package[?(@.name=="oxide-player")].version\njson\tfrontend/package.json\t$.version' ]]
 [[ "$(jq -r 'keys | join(" ")' "$manifest")" == "." ]]
 [[ -f backend/CHANGELOG.md ]]
 [[ -f frontend/package.json ]]
